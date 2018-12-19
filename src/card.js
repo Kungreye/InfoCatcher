e7dea3a8e4b11caab8cb6ee01bc57ff81672129f
Kungreye <kungreye@sina.com>
2018-12-19 21:48:04 +0800

    add card.js, update post.js

---
+++ src/card.js
@@ -0,0 +1,52 @@
+var $likeBtn = $('.like-button');
+var $isLiked = $likeBtn.hasClass('liked');
+var $collectBtn = $('.collect-button');
+var $isCollected = $collectBtn.hasClass('collected');
+
+
+$likeBtn.on('click', (event) => {
+    let $this = $(event.currentTarget);
+    let url = $this.data('url');
+
+    $.ajax({
+        url: `/api/${url}`,
+        type: $isLiked? 'DELETE' : 'POST',
+        data: {},
+        success: function(rs) {
+            if (!rs.r) {
+                let isLiked = rs.data.is_liked;
+                if ($isLiked != isLiked) {
+                    $isLiked = isLiked;
+                    $this.toggleClass('liked');
+                }
+            }
+            else {
+                alert('Your Upvote operation failed, please try later.');
+            }
+        }
+    });
+});
+
+
+$collectBtn.on('click', (event) => {
+    let $this = $(event.currentTarget);
+    let url = $this.data('url');
+
+    $.ajax({
+        url: `/api/${url}`,
+        type: $isCollected? 'DELETE' : 'POST',
+        data: {},
+        success: function(rs) {
+            if (!rs.r) {
+                let isCollected = rs.data.is_collected;
+                if ($isCollected != isCollected) {
+                    $isCollected = isCollected;
+                    $this.toggleClass('collected');
+                }
+            }
+            else {
+                alert('Your Collect operation failed, please try later.');
+            }
+        }
+    });
+});
--- src/post.js
+++ src/post.js
@@ -1,4 +1,32 @@
 import "./scss/post.scss";
 import "./css/font.css";
 
-console.log('Hello world!');
+import "./card";
+
+
+var $comments = $('#comments');
+var $submitBtn = $('#comment-submit');
+var $commentForm = $('#comment-form');
+
+$commentForm.on('submit', (event) => {
+    event.preventDefault;
+    let $this = $(event.currentTarget);
+    let $content = $this.find('#comment-content');
+    let url = $this.data('url');
+
+    $.ajax({
+        url: `/api/${url}`,
+        type: 'POST',
+        data: {content: $content.val()},
+        success: function(rs) {
+            if (!rs.r) {
+                $content.val('');   // clear <textarea>
+                $(rs.data.html).hide().prepend($comments).fadeIn(1000);
+            }
+            else {
+                alert('Your Comment operation failed, please try later.');
+            }
+        }
+    });
+    return false;
+});
